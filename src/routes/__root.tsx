2342bde5b611f4b283923800a4cd4613408c3574
unknown <andreas3739@gmail.com>
2025-05-29 16:03:19 +0700

    vite tanstack router init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,25 @@
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+import Header from '../components/Header'
+
+import type { QueryClient } from '@tanstack/react-query'
+import TanStackQueryLayout from '@/integrations/tanstack-query/layout'
+
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <Header />
+
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <TanStackQueryLayout />
+    </>
+  ),
+})
